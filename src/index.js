1542c2800ee3ee21b361e6c261853f54fbb785b4
elianbarci <eli.barci@gmail.com>
2021-04-04 21:51:35 -0300

    Se guarda en el board

--- src/index.js
+++ src/index.js
@@ -3,38 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
+  
   render() {
     return (
-      <button className="square" onClick={ () => this.setState({value: 'X'}) } >
-        {this.state.value}
+      <button className="square" onClick={ () => this.props.onClick() } >
+        {this.props.value}
       </button>
     );
   }
-
-  // changeMark(){
-
-  //   if(this.state.value == 'X'){
-  //     this.setState({ value: '' })
-  //   } else {
-  //     this.setState({ value: 'X' })
-  //   }
-
-  // }
 
 }
   
   class Board extends React.Component {
 
     renderSquare(i) {
-      return <Square value={i}/>;
+      return <Square value={this.state.squares[i]} onClick={ () => this.handleClick(i) }/>;
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
     }
 
     constructor(props){
